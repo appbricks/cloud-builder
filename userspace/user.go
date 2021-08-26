6eb5c39c803feca5a8c5a77039f9b4e58bbc41c5
Mevan Samaratunga <mevansam@gmail.com>
2021-08-26 14:40:36 -0400

    added space user type

--- userspace/user.go
+++ userspace/user.go
@@ -10,10 +10,18 @@ type User struct {
 	WGPrivateKey string `json:"wgPrivateKey,omitempty"`
 	WGPublickKey string `json:"wgPublickKey,omitempty"`
 
-	// active devices for this users
-	Devices []*Device `json:"devices,omitempty"`
-
 	// indicates if user is active 
 	// for the device in context
 	Active bool `json:"active"`
+}
+
+type SpaceUser struct {
+	UserID string `json:"userID"`
+	Name   string `json:"name"`
+
+	IsOwner    bool   `json:"isOwner"`
+	AccessType string `json:"accessType"`
+
+	// active devices for this users
+	Devices []*Device `json:"devices,omitempty"`
 }
