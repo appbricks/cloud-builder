2e07855480419bbb296a80cfbde168a8e3cd377a
Mevan Samaratunga <mevansam@gmail.com>
2022-02-23 13:05:07 -0500

    maintain a timestamp of when the user keys were created or imported

--- userspace/user.go
+++ userspace/user.go
@@ -1,11 +1,14 @@
 package userspace
 
+import "time"
+
 type User struct {
 	UserID string `json:"userID"`
 	Name   string `json:"name"`
 
-	RSAPrivateKey string `json:"rsaPrivateKey,omitempty"`
-	RSAPublicKey  string `json:"rsaPublicKey,omitempty"`
+	KeyTimestamp  time.Duration `json:"keyTimestamp,omitempty"`
+	RSAPrivateKey string        `json:"rsaPrivateKey,omitempty"`
+	RSAPublicKey  string        `json:"rsaPublicKey,omitempty"`
 
 	// indicates if user is active 
 	// for the device in context
