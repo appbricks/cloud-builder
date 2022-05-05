6ea954408548c1c4507ddaea73bdd739503caaee
Mevan Samaratunga <mevansam@gmail.com>
2022-05-05 18:50:25 -0400

    corrected comment

--- auth/role.go
+++ auth/role.go
@@ -38,14 +38,14 @@ func RoleFromContext(
 	//
 	// Admin - user logged in to device is also the device and space owner. 
 	//
-	// Manager - user logged in to device IS the device owner, but has been 
+	// Manager - user logged in to device IS the device owner, and has been 
 	//           granted admin access to the space. the user logged in to 
 	//           device IS NOT the device owner and has admin access to the 
 	//           space. space owners are also space admins by default so the
 	//           user that falls into this role could be the space owner.
 	//
 	// Guest - all other users that have access to the space
-	//  
+	//
 	ownerUserID, isOwnerConfigured := deviceContext.GetOwnerUserID()
 	if isOwnerConfigured && ownerUserID == deviceContext.GetLoggedInUserID() {
 		if spaceNode == nil || spaceNode.IsSpaceOwned() {
